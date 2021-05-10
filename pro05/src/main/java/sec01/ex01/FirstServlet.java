1c29fdd9fa34eb91bdef50263c42ea14b0ecb3ac
yejiwoo <5walls5@naver.com>
2021-05-10 18:22:32 +0900

    0510:3rd-Mon

---
+++ pro05/src/main/java/sec01/ex01/FirstServlet.java
@@ -0,0 +1,52 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+       
+	@Override
+	public void init() throws ServletException{
+		System.out.println("FirstServlet: init 메서도 호출");
+	}
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("FirstServlet: doGet 메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("FirstServlet: destroy 메서도 호출");
+	}
+
+}
---
+++ pro05/src/main/java/sec01/ex01/SecondServlet.java
@@ -0,0 +1,52 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+       
+	@Override
+	public void init() throws ServletException{
+		System.out.println("SecondServlet: init 메서도 호출");
+	}
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("SecondServlet: doGet 메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+	
+	@Override
+	public void destroy() {
+		System.out.println("SecondServlet: destroy 메서도 호출");
+	}
+
+}
